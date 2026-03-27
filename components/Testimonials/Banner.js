c0d2f5a5f59559e1690bb6f54b5c94b1b546b667
dumpalavenkatatejesh <dumpalavenkatatejesh@gmail.com>
2026-03-27 09:41:04 +0530

    testimonial fixes

--- components/Testimonials/Banner.js
+++ components/Testimonials/Banner.js
@@ -1,5 +1,5 @@
-"use client"
-import React, { useContext, useEffect, useState } from 'react';
+"use client";
+import React, { useContext, useEffect, useState } from "react";
 import { LanguageContext } from "../../app/context/LanguageContext";
 
 export default function Banner() {
@@ -15,18 +15,18 @@ export default function Banner() {
 
   return (
     <div
-      className="relative md:bg-[url('/PracticeArea/PracticeAreas.png')] bg-[url('/PracticeArea/PracticeAreas.png')] bg-cover bg-center"
+      className="relative bg-[url('/PracticeArea/PracticeAreas.png')] bg-cover bg-center md:bg-[url('/PracticeArea/PracticeAreas.png')]"
       style={{ height: "550px" }}
     >
       <div
         className="absolute flex w-full items-center justify-center"
         style={{
           top: navHeight ? `${(550 - navHeight) / 1.8 + navHeight}px` : "50%",
-          transform: "translateY(-50%)"
+          transform: "translateY(-50%)",
         }}
       >
-        <h1 className="md:text-3xl text-2xl font-bold text-white bg-black/50 px-4 py-2">
-        {translations.testmonialBannerTitle.testmonialBannerName}
+        <h1 className="bg-black/50 px-4 py-2 text-2xl font-bold text-white md:text-3xl">
+          {translations.testmonialBannerTitle.testmonialBannerName}
         </h1>
       </div>
     </div>
