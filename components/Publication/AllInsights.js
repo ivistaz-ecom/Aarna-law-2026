c4403a1e1a152cd0e87e2c7c438112d2796c6ce6
krishnakkp <kanhu.krishnapatra@gmail.com>
2026-05-05 16:49:09 +0530

    Publication2 Live

--- components/Publication/AllInsights.js
+++ components/Publication/AllInsights.js
@@ -149,6 +149,7 @@ function AllInsights({ searchTerm, initialData = [] }) {
                   __html: stripHTMLAndLimit(item.content.rendered),
                 }}
               ></p>
+<<<<<<< Updated upstream
               {item.acf.publication_url ? (
                 <>
                   {showForm ? (
@@ -172,6 +173,29 @@ function AllInsights({ searchTerm, initialData = [] }) {
                     </Link>
                   )}
                 </>
+=======
+              {item.acf.publication_url === "https://publications2.aarnalaw.com/art-law-2026" ? (
+                <Link
+                  href={item.acf.publication_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-semibold text-custom-red"
+                >
+                  Read more
+                </Link>
+              ) : item.acf.publication_url ? (
+                <Link
+                  href="#"
+                  className="font-semibold text-custom-red"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setShowForm(true);
+                    setSelectedItem(item);
+                  }}
+                >
+                  Read more
+                </Link>
+>>>>>>> Stashed changes
               ) : (
                 <Link
                   href={`/publications/${item.slug}`}
